c724ffae4363e038a37aab544f64142e1948162c
Paul <paulmakles@gmail.com>
2021-06-30 21:28:56 +0100

    View roles in server settings.

--- src/pages/settings/ServerSettings.tsx
+++ src/pages/settings/ServerSettings.tsx
@@ -3,7 +3,7 @@ import Category from "../../components/ui/Category";
 import { GenericSettings } from "./GenericSettings";
 import { useServer } from "../../context/revoltjs/hooks";
 import { Route, useHistory, useParams } from "react-router-dom";
-import { ListUl, Share, Group } from "@styled-icons/boxicons-regular";
+import { ListUl, Share, Group, ListCheck } from "@styled-icons/boxicons-regular";
 import { XSquare } from "@styled-icons/boxicons-solid";
 import RequiresOnline from "../../context/revoltjs/RequiresOnline";
 
@@ -11,6 +11,7 @@ import { Overview } from "./server/Overview";
 import { Members } from "./server/Members";
 import { Invites } from "./server/Invites";
 import { Bans } from "./server/Bans";
+import { Roles } from "./server/Roles";
 
 export default function ServerSettings() {
     const { server: sid } = useParams<{ server: string; }>();
@@ -49,12 +50,18 @@ export default function ServerSettings() {
                     id: 'bans',
                     icon: <XSquare size={20} />,
                     title: "Bans"
+                },
+                {
+                    id: 'roles',
+                    icon: <ListCheck size={20} />,
+                    title: "Roles"
                 }
             ]}
             children={[
                 <Route path="/server/:server/settings/members"><RequiresOnline><Members server={server} /></RequiresOnline></Route>,
                 <Route path="/server/:server/settings/invites"><RequiresOnline><Invites server={server} /></RequiresOnline></Route>,
                 <Route path="/server/:server/settings/bans"><RequiresOnline><Bans server={server} /></RequiresOnline></Route>,
+                <Route path="/server/:server/settings/roles"><RequiresOnline><Roles server={server} /></RequiresOnline></Route>,
                 <Route path="/"><Overview server={server} /></Route>
             ]}
             category="server_pages"
