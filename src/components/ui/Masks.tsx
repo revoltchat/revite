a46d58f8f9cf998a00aec81709c6f808b182ebb8
Paul <paulmakles@gmail.com>
2021-07-08 14:07:25 +0100

    Merge session icons together.

--- src/components/ui/Masks.tsx
+++ src/components/ui/Masks.tsx
@@ -12,6 +12,10 @@ export default function Masks() {
                     <rect x="0" y="0" width="32" height="32" fill="white" />
                     <circle cx="27" cy="27" r="7" fill={"black"} />
                 </mask>
+                <mask id="session">
+                    <rect x="0" y="0" width="32" height="32" fill="white" />
+                    <circle cx="26" cy="30" r="12" fill={"black"} />
+                </mask>
                 <mask id="overlap">
                     <rect x="0" y="0" width="32" height="32" fill="white" />
                     <circle cx="32" cy="16" r="18" fill={"black"} />
