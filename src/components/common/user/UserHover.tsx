c993ece1092c5f5ffdbe973c1ada20359d675d7c
nizune <9-nizune@users.noreply.gitlab.insrt.uk>
2021-07-10 00:09:49 +0200

    Small changes to userhover

--- src/components/common/user/UserHover.tsx
+++ src/components/common/user/UserHover.tsx
@@ -1,3 +1,4 @@
+import { InfoCircle } from "@styled-icons/boxicons-regular";
 import { Children } from "../../../types/Preact";
 import { Username } from "./UserShort";
 import styled from "styled-components";
@@ -15,13 +16,23 @@ const Base = styled.div`
     flex-direction: column;
 
     .username {
+        font-size: 13px;
         font-weight: 600;
     }
 
     .status {
+        font-size: 11px;
         overflow: hidden;
         white-space: nowrap;
         text-overflow: ellipsis;
+    }
+
+    .tip {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+        margin-top: 2px;
+        color: var(--secondary-foreground);
     }
 `;
 
@@ -33,6 +44,7 @@ export default function UserHover({ user, children }: Props) {
                 <span className="status">
                     <UserStatus user={user} />
                 </span>
+                {/*<div className="tip"><InfoCircle size={13}/>Right-click on the avatar to access the quick menu</div>*/}
             </Base>
         }>
             { children }
