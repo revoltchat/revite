3b19299cd76077ead44af075dd3bac962fe44755
Ryan Alexander <38785445+ryanalexander@users.noreply.github.com>
2021-08-17 19:33:09 +1000

    Lowered contrast threshold

--- src/pages/settings/panes/Appearance.tsx
+++ src/pages/settings/panes/Appearance.tsx
@@ -461,5 +461,5 @@ function getContrastingColour(hex: string) {
     const g = parseInt(hex.substr(2, 2), 16);
     const b = parseInt(hex.substr(4, 2), 16);
     const cc = (r * 299 + g * 587 + b * 114) / 1000;
-    return cc >= 128 ? "black" : "white";
+    return cc >= 75 ? "black" : "white";
 }
