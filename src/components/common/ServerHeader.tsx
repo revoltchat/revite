ee3d5bef460d9dc9cd67d353c509b5031234a9d4
Ryan Alexander <w.ryan@stelch.com>
2021-08-18 08:51:32 +1000

    Introduced verified server badges. Automatically shown on any server created by insert

--- src/components/common/ServerHeader.tsx
+++ src/components/common/ServerHeader.tsx
@@ -5,8 +5,12 @@ import { ServerPermission } from "revolt.js/dist/api/permissions";
 import { Server } from "revolt.js/dist/maps/Servers";
 import styled from "styled-components";
 
+import { Text } from "preact-i18n";
+
 import Header from "../ui/Header";
 import IconButton from "../ui/IconButton";
+
+import Tooltip from "./Tooltip";
 
 interface Props {
     server: Server;
@@ -27,6 +31,28 @@ export default observer(({ server }: Props) => {
             style={{
                 background: bannerURL ? `url('${bannerURL}')` : undefined,
             }}>
+            {server.owner === "01EX2NCWQ0CHS3QJF0FEQS1GR4" ? (
+                <Tooltip content={"Official Server"} placement={"bottom-start"}>
+                    <svg width="20" height="20">
+                        <image
+                            xlinkHref="/assets/verified.svg"
+                            height="20"
+                            width="20"
+                        />
+                        <image
+                            xlinkHref="/assets/badges/developer.svg"
+                            height="15"
+                            width="15"
+                            x="3"
+                            y="3"
+                            style={
+                                "justify-content: center; align-items: center;"
+                            }
+                        />
+                    </svg>
+                </Tooltip>
+            ) : undefined}
+
             <ServerName>{server.name}</ServerName>
             {(server.permission & ServerPermission.ManageServer) > 0 && (
                 <div className="actions">
