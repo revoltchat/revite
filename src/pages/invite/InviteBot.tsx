19220ede6677d1fd1edd2de53d27173ed68f4a35
div2005 <div2005@tuta.io>
2021-08-19 16:23:03 +0300

    Update for invite page

--- src/pages/invite/InviteBot.tsx
+++ src/pages/invite/InviteBot.tsx
@@ -1,3 +1,5 @@
+import { Bot as BotIcon } from "@styled-icons/boxicons-solid";
+
 import { useParams } from "react-router-dom";
 import { Route } from "revolt.js/dist/api/routes";
 
@@ -5,12 +7,14 @@ import { useEffect, useState } from "preact/hooks";
 
 import { useClient } from "../../context/revoltjs/RevoltClient";
 
+import styles from "./Invite.module.scss";
+import Header from "../../components/ui/Header";
+
 import UserIcon from "../../components/common/user/UserIcon";
 import Button from "../../components/ui/Button";
 import ComboBox from "../../components/ui/ComboBox";
 import Overline from "../../components/ui/Overline";
 import Preloader from "../../components/ui/Preloader";
-import Tip from "../../components/ui/Tip";
 
 export default function InviteBot() {
     const { id } = useParams<{ id: string }>();
@@ -27,58 +31,69 @@ export default function InviteBot() {
     const [group, setGroup] = useState("none");
 
     return (
-        <div style={{ padding: "6em" }}>
-            <Tip warning hideSeparator>
-                This section is under construction.
-            </Tip>
-            {typeof data === "undefined" && <Preloader type="spinner" />}
-            {data && (
-                <>
-                    <UserIcon size={64} attachment={data.avatar} />
-                    <h1>{data.username}</h1>
-                    {data.description && <p>{data.description}</p>}
-                    <Overline type="subtle">Add to server</Overline>
-                    <ComboBox
-                        value={server}
-                        onChange={(e) => setServer(e.currentTarget.value)}>
-                        <option value="none">un selected</option>
-                        {[...client.servers.values()].map((server) => (
-                            <option value={server._id} key={server._id}>
-                                {server.name}
-                            </option>
-                        ))}
-                    </ComboBox>
-                    <Button
-                        contrast
-                        onClick={() =>
-                            server !== "none" &&
-                            client.bots.invite(data._id, { server })
-                        }>
-                        add
-                    </Button>
-                    <Overline type="subtle">Add to group</Overline>
-                    <ComboBox
-                        value={group}
-                        onChange={(e) => setGroup(e.currentTarget.value)}>
-                        <option value="none">un selected</option>
-                        {[...client.channels.values()]
-                            .filter((x) => x.channel_type === "Group")
-                            .map((channel) => (
-                                <option value={channel._id} key={channel._id}>
-                                    {channel.name}
-                                </option>
-                            ))}
-                    </ComboBox>
-                    <Button
-                        contrast
-                        onClick={() =>
-                            group !== "none" &&
-                            client.bots.invite(data._id, { group })
-                        }>
-                        add
-                    </Button>
-                </>
+        // ! FIXME: add i18n translations
+        <div>
+
+            <Header placement="primary">
+                <BotIcon size={24} />
+                Invite Bot
+            </Header>
+
+            <div className={styles.inviteBot}>
+                {typeof data === "undefined" && <Preloader type="spinner" />}
+                {data && (
+                    <>
+                        <UserIcon size={64} attachment={data.avatar} />
+                        <h1>{data.username}</h1>
+                        {data.description && <p>{data.description}</p>}
+
+                        <div className={styles.type}>
+                            <Overline type="subtle">Add to server</Overline>
+                            <ComboBox
+                                value={server}
+                                onChange={(e) => setServer(e.currentTarget.value)}>
+                                <option value="none">Server not selected..</option>
+                                {[...client.servers.values()].map((server) => (
+                                    <option value={server._id} key={server._id}>
+                                        {server.name}
+                                    </option>
+                                ))}
+                            </ComboBox>
+                            <Button
+                                contrast
+                                onClick={() =>
+                                    server !== "none" &&
+                                    client.bots.invite(data._id, { server })
+                                }>
+                                Add
+                            </Button>
+                        </div>
+                        <div className={styles.type}>
+                            <Overline type="subtle">Add to group</Overline>
+                            <ComboBox
+                                value={group}
+                                onChange={(e) => setGroup(e.currentTarget.value)}>
+                                <option value="none">Group not selected..</option>
+                                {[...client.channels.values()]
+                                    .filter((x) => x.channel_type === "Group")
+                                    .map((channel) => (
+                                        <option value={channel._id} key={channel._id}>
+                                            {channel.name}
+                                        </option>
+                                    ))}
+                            </ComboBox>
+                            <Button
+                                contrast
+                                onClick={() =>
+                                    group !== "none" &&
+                                    client.bots.invite(data._id, { group })
+                                }>
+                                Add
+                            </Button>
+                        </div>
+                    </>
             )}
+            </div>
         </div>
     );
 }
