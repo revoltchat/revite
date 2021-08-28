8354758cae6a0ff1949609fc5ddc8deda71fee02
Ryan Alexander <w.ryan@stelch.com>
2021-08-29 09:56:20 +1000

    Add external link warning

---
+++ src/context/intermediate/modals/ExternalLinkPrompt.tsx
@@ -0,0 +1,32 @@
+import { Text } from "preact-i18n";
+
+import Modal from "../../../components/ui/Modal";
+
+interface Props {
+    onClose: () => void;
+    link: string;
+}
+
+export function ExternalLinkModal({ onClose, link }: Props) {
+    return (
+        <Modal
+            visible={true}
+            onClose={onClose}
+            title={<Text id={"app.special.modals.external_links.title"} />}
+            actions={[
+                {
+                    onClick: ()=>{window.open(link, "_blank");},
+                    confirmation: true,
+                    children: "Continue",
+                },
+                {
+                    onClick: onClose,
+                    confirmation: false,
+                    children: "Cancel",
+                },
+            ]}>
+            <Text id={"app.special.modals.external_links.short"} /> <br />
+            <a>{link}</a>
+        </Modal>
+    );
+}
