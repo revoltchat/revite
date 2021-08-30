144e4db7c76e4e5f47db3177825e4ad5e1b62697
Snazzah <7025343+Snazzah@users.noreply.github.com>
2021-08-30 16:18:42 +0000

    Initial bot page stylings

--- src/pages/settings/panes/MyBots.tsx
+++ src/pages/settings/panes/MyBots.tsx
@@ -1,17 +1,26 @@
+import { Key, Clipboard, Globe } from "@styled-icons/boxicons-regular";
+import { LockAlt } from "@styled-icons/boxicons-solid";
 import { observer } from "mobx-react-lite";
 import { Bot } from "revolt-api/types/Bots";
+import styled from "styled-components";
 
+import styles from "./Panes.module.scss";
+import { Text } from "preact-i18n";
 import { useEffect, useState } from "preact/hooks";
+
+import { stopPropagation } from "../../../lib/stopPropagation";
 
 import { useIntermediate } from "../../../context/intermediate/Intermediate";
 import { useClient } from "../../../context/revoltjs/RevoltClient";
 
-import UserShort from "../../../components/common/user/UserShort";
+import Tooltip from "../../../components/common/Tooltip";
+import UserIcon from "../../../components/common/user/UserIcon";
 import Button from "../../../components/ui/Button";
 import Checkbox from "../../../components/ui/Checkbox";
 import InputBox from "../../../components/ui/InputBox";
 import Overline from "../../../components/ui/Overline";
 import Tip from "../../../components/ui/Tip";
+import CategoryButton from "../../../components/ui/fluent/CategoryButton";
 
 interface Data {
     _id: string;
@@ -19,6 +28,21 @@ interface Data {
     public: boolean;
     interactions_url?: string;
 }
+
+const BotBadge = styled.div`
+    display: inline-block;
+
+    height: 1.3em;
+    padding: 0px 4px;
+    font-size: 0.7em;
+    user-select: none;
+    margin-inline-start: 2px;
+    text-transform: uppercase;
+
+    color: var(--foreground);
+    background: var(--accent);
+    border-radius: calc(var(--border-radius) / 2);
+`;
 
 function BotEditor({ bot }: { bot: Data }) {
     const client = useClient();
@@ -78,10 +102,11 @@ export const MyBots = observer(() => {
     }, []);
 
     const [name, setName] = useState("");
-    const { writeClipboard } = useIntermediate();
+    const [editMode, setEditMode] = useState(false);
+    const { writeClipboard, openScreen } = useIntermediate();
 
     return (
-        <div>
+        <div className={styles.myBots}>
             <Tip warning hideSeparator>
                 This section is under construction.
             </Tip>
@@ -113,16 +138,80 @@ export const MyBots = observer(() => {
                         key={bot._id}
                         style={{
                             background: "var(--secondary-background)",
-                            margin: "8px",
+                            margin: "8px 0",
                             padding: "12px",
                         }}>
-                        <UserShort user={user} />
-                        <p>
-                            token:{" "}
-                            <code style={{ userSelect: "all" }}>
-                                {bot.token}
-                            </code>
-                        </p>
+                        <div className={styles.infoheader}>
+                            <div className={styles.container}>
+                                <UserIcon
+                                    className={styles.avatar}
+                                    target={user!}
+                                    size={48}
+                                    onClick={() =>
+                                        openScreen({
+                                            id: "profile",
+                                            user_id: user!._id,
+                                        })
+                                    }
+                                />
+                                <div className={styles.userDetail}>
+                                    <div className={styles.userName}>
+                                        {user!.username}{" "}
+                                        <BotBadge>
+                                            <Text id="app.main.channel.bot" />
+                                        </BotBadge>
+                                    </div>
+
+                                    <div className={styles.userid}>
+                                        <Tooltip
+                                            content={
+                                                <Text id="app.special.copy" />
+                                            }>
+                                            <a
+                                                onClick={() =>
+                                                    writeClipboard(
+                                                        client.user!._id,
+                                                    )
+                                                }>
+                                                {client.user!._id}
+                                            </a>
+                                        </Tooltip>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <Tooltip content={bot.public ? "Bot is public. Anyone can invite it." : "Bot is private. Only you can invite it."}>
+                                {bot.public ? <Globe size={24} /> : <LockAlt size={24} />}
+                            </Tooltip>
+                            {/* <Button onClick={() => switchPage("profile")} contrast>
+                                <Text id="app.settings.pages.profile.edit_profile" />
+                            </Button> */}
+                        </div>
+                        <CategoryButton
+                            account
+                            icon={<Key size={24} />}
+                            onClick={() => writeClipboard(bot.token)}
+                            description={
+                                <>
+                                    {"••••••••••••••••••••••••••••••••••••"}{" "}
+                                    <a
+                                        onClick={(ev) =>
+                                            stopPropagation(
+                                                ev,
+                                                openScreen({
+                                                    id: "token_reveal",
+                                                    token: bot.token,
+                                                    username: user!.username,
+                                                }),
+                                            )
+                                        }>
+                                        <Text id="app.special.modals.actions.reveal" />
+                                    </a>
+                                </>
+                            }
+                            action={<Clipboard size={18} />}>
+                            Token
+                        </CategoryButton>
                         <BotEditor
                             bot={{
                                 ...bot,
