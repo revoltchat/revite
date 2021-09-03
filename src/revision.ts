c302eade177eb349895b68c9ee37390d676baea3
Paul <paulmakles@gmail.com>
2021-09-03 10:30:27 +0100

    Update source links from GitLab to GitHub.

--- src/revision.ts
+++ src/revision.ts
@@ -1,7 +1,6 @@
 /* eslint-disable */
 // Strings needs to be explictly stated here as they can cause type issues elsewhere.
 
-export const REPO_URL: string =
-    "https://gitlab.insrt.uk/revolt/revite/-/commit";
+export const REPO_URL: string = "https://github.com/revoltchat/revite/commit";
 export const GIT_REVISION: string = "__GIT_REVISION__";
 export const GIT_BRANCH: string = "__GIT_BRANCH__";
