25e49b63faed179aef8cc784de1a80068aae75a7
Ryan Alexander <38785445+ryanalexander@users.noreply.github.com>
2021-09-03 19:35:02 +1000

    Rename "ios" & "fxios" to "Safari" and "Firefox" for browser name (#119)

--- src/pages/login/forms/FormLogin.tsx
+++ src/pages/login/forms/FormLogin.tsx
@@ -1,11 +1,7 @@
-import { detect } from "detect-browser";
-import { useHistory } from "react-router-dom";
-
-import { useContext } from "preact/hooks";
-
-import { OperationsContext } from "../../../context/revoltjs/RevoltClient";
-
 import { Form } from "./Form";
+import { detect } from "detect-browser";
+import { useContext } from "preact/hooks";
+import { OperationsContext } from "../../../context/revoltjs/RevoltClient";
 
 export function FormLogin() {
     const { login } = useContext(OperationsContext);
@@ -14,15 +10,21 @@ export function FormLogin() {
     return (
         <Form
             page="login"
-            callback={async (data) => {
+            callback={async data => {
                 const browser = detect();
                 let device_name;
                 if (browser) {
-                    const { name, os } = browser;
+                    let { name } = browser;
+                    const { os } = browser;
                     if (window.isNative) {
                         device_name = `Revolt Desktop on ${os}`;
                     } else {
-                        device_name = `${name} on ${os}`;
+                      if(name === "ios") {
+                          name = "safari";
+                      }else if(name === "fxios") {
+                          name = "firefox";
+                      }
+                      device_name = `${name} on ${os}`;
                     }
                 } else {
                     device_name = "Unknown Device";
