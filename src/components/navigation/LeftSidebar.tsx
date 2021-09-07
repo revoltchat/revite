ed56967e10ea998239b67fd6bced4ebf0e20bd4d
brecert <me@bree.dev>
2021-09-07 10:51:46 -0400

    Add the ability to hide the channel sidebar (the left one)

--- src/components/navigation/LeftSidebar.tsx
+++ src/components/navigation/LeftSidebar.tsx
@@ -4,27 +4,31 @@ import SidebarBase from "./SidebarBase";
 import HomeSidebar from "./left/HomeSidebar";
 import ServerListSidebar from "./left/ServerListSidebar";
 import ServerSidebar from "./left/ServerSidebar";
+import { useSelector } from "react-redux";
+import { State } from "../../redux";
 
 export default function LeftSidebar() {
+    const isOpen = useSelector((state: State) => state.sectionToggle['sidebar_channels'] ?? true)
+
     return (
         <SidebarBase>
             <Switch>
                 <Route path="/settings" />
                 <Route path="/server/:server/channel/:channel">
                     <ServerListSidebar />
-                    <ServerSidebar />
+                    {isOpen && <ServerSidebar />}
                 </Route>
                 <Route path="/server/:server">
                     <ServerListSidebar />
-                    <ServerSidebar />
+                    {isOpen && <ServerSidebar />}
                 </Route>
                 <Route path="/channel/:channel">
                     <ServerListSidebar />
-                    <HomeSidebar />
+                    {isOpen && <HomeSidebar />}
                 </Route>
                 <Route path="/">
                     <ServerListSidebar />
-                    <HomeSidebar />
+                    {isOpen && <HomeSidebar />}
                 </Route>
             </Switch>
         </SidebarBase>
