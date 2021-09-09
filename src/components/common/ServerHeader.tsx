16f3958b1744e8ae0a097a35774e3d5ae5e0354d
kaname-png <inmortaldragonxspace@outlook.com>
2021-09-09 17:35:31 -0600

    fix(ServerHeader): removed degraded effect on servers without banners

--- src/components/common/ServerHeader.tsx
+++ src/components/common/ServerHeader.tsx
@@ -3,7 +3,7 @@ import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
 import { ServerPermission } from "revolt.js/dist/api/permissions";
 import { Server } from "revolt.js/dist/maps/Servers";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import { Text } from "preact-i18n";
 
@@ -23,19 +23,24 @@ const ServerName = styled.div`
     text-overflow: ellipsis;
 `;
 
-const GradientHeader = styled.div`
+const GradientHeader = styled.div<{ gradient: boolean }>`
     height: 100%;
     width: 100%;
     display: flex;
     flex-direction: row;
     align-items: end;
     padding: 15px;
-    background: linear-gradient(
-        0deg,
-        rgba(var(--secondary-background-rgba), 1) 0%,
-        rgba(var(--secondary-background-rgba), 0.8) 40%,
-        rgba(var(--secondary-background-rgba), 0) 100%
-    );
+
+    ${(props) =>
+        props.gradient &&
+        css`
+            background: linear-gradient(
+                0deg,
+                rgba(var(--secondary-background-rgba), 1) 0%,
+                rgba(var(--secondary-background-rgba), 0.8) 40%,
+                rgba(var(--secondary-background-rgba), 0) 100%
+            );
+        `}
 `;
 
 export default observer(({ server }: Props) => {
@@ -49,7 +54,7 @@ export default observer(({ server }: Props) => {
             style={{
                 background: bannerURL ? `url('${bannerURL}')` : undefined,
             }}>
-            <GradientHeader>
+            <GradientHeader gradient={typeof bannerURL !== "undefined"}>
                 {server.owner === "01EX2NCWQ0CHS3QJF0FEQS1GR4" ? (
                     <Tooltip
                         content={"Official Server"}
