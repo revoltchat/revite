345792b66f4b63d141de9961846d1104ceaf250c
Kaname <56084970+kaname-png@users.noreply.github.com>
2021-09-09 17:14:41 -0600

    fix(conflicts): again again again bruh

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,33 +1,30 @@
 import { Home as HomeIcon } from "@styled-icons/boxicons-solid";
 import { Link } from "react-router-dom";
-import styled, { css } from "styled-components";
 
 import styles from "./Home.module.scss";
 import { Text } from "preact-i18n";
-import { useState } from "preact/hooks";
-
-import { isTouchscreenDevice } from "../../lib/isTouchscreenDevice";
-
-import { dispatch, getState } from "../../redux";
 
 import wideSVG from "../../assets/wide.svg";
 import Emoji from "../../components/common/Emoji";
 import Tooltip from "../../components/common/Tooltip";
 import Header from "../../components/ui/Header";
 import CategoryButton from "../../components/ui/fluent/CategoryButton";
+import { dispatch, getState } from "../../redux";
+import { useState } from "preact/hooks";
+import styled, { css } from "styled-components";
+import { isTouchscreenDevice } from "../../lib/isTouchscreenDevice";
 
 const CHANNELS_SIDEBAR_KEY = "sidebar_channels";
 
 const IconConainer = styled.div`
     cursor: pointer;
     color: var(--secondary-foreground);
-    ${!isTouchscreenDevice &&
-    css`
+    ${!isTouchscreenDevice && css`
         &:hover {
             color: var(--foreground);
         }
     `}
-`;
+`
 
 export default function Home() {
     const [showChannels, setChannels] = useState(
@@ -36,7 +33,7 @@ export default function Home() {
 
     const toggleChannelSidebar = () => {
         if (isTouchscreenDevice) {
-            return;
+            return
         }
 
         setChannels(!showChannels);
@@ -53,14 +50,12 @@ export default function Home() {
                 id: CHANNELS_SIDEBAR_KEY,
             });
         }
-    };
+    }
 
     return (
         <div className={styles.home}>
-            <Header placement="primary" padding={true}>
-                <IconConainer onClick={toggleChannelSidebar}>
-                    <HomeIcon size={24} />
-                </IconConainer>
+            <Header placement="primary">
+                <IconConainer onClick={toggleChannelSidebar} ><HomeIcon size={24} /></IconConainer>
                 <Text id="app.navigation.tabs.home" />
             </Header>
             <h3>
