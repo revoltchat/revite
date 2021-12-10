13f93e60f63f1b78d2120121fc0f48ba3f8bedf8
Paul Makles <paulmakles@gmail.com>
2021-12-10 13:55:05 +0000

    feat(mobx): write experiments, lastOpened and localeOptions stores

---
+++ src/mobx/stores/LastOpened.ts
@@ -0,0 +1,57 @@
+import { action, computed, makeAutoObservable, ObservableMap } from "mobx";
+
+import Persistent from "../Persistent";
+
+interface Data {
+    server?: Record<string, string>;
+}
+
+/**
+ * Keeps track of the last open channels, tabs, etc.
+ * Handles providing good UX experience on navigating
+ * back and forth between different parts of the app.
+ */
+export default class Experiments implements Persistent<Data> {
+    private server: ObservableMap<string, string>;
+
+    /**
+     * Construct new Experiments store.
+     */
+    constructor() {
+        this.server = new ObservableMap();
+        makeAutoObservable(this);
+    }
+
+    // eslint-disable-next-line require-jsdoc
+    toJSON() {
+        return {
+            server: this.server,
+        };
+    }
+
+    // eslint-disable-next-line require-jsdoc
+    @action hydrate(data: Data) {
+        if (data.server) {
+            Object.keys(data.server).forEach((key) =>
+                this.server.set(key, data.server![key]),
+            );
+        }
+    }
+
+    /**
+     * Get last opened channel in a server.
+     * @param server Server ID
+     */
+    @computed get(server: string) {
+        return this.server.get(server);
+    }
+
+    /**
+     * Set last opened channel in a server.
+     * @param server Server ID
+     * @param channel Channel ID
+     */
+    @action enable(server: string, channel: string) {
+        this.server.set(server, channel);
+    }
+}
---
+++ src/mobx/stores/LocaleOptions.ts
@@ -0,0 +1,84 @@
+import { action, computed, makeAutoObservable } from "mobx";
+
+import { Language, Languages } from "../../context/Locale";
+
+import Persistent from "../Persistent";
+
+interface Data {
+    lang: Language;
+}
+
+/**
+ * Detect the browser language or match given language.
+ * @param lang Language to find
+ * @returns Matched Language
+ */
+export function findLanguage(lang?: string): Language {
+    if (!lang) {
+        if (typeof navigator === "undefined") {
+            lang = Language.ENGLISH;
+        } else {
+            lang = navigator.language;
+        }
+    }
+
+    const code = lang.replace("-", "_");
+    const short = code.split("_")[0];
+
+    const values = [];
+    for (const key in Language) {
+        const value = Language[key as keyof typeof Language];
+
+        // Skip alternative/joke languages
+        if (Languages[value].cat === "alt") continue;
+
+        values.push(value);
+        if (value.startsWith(code)) {
+            return value as Language;
+        }
+    }
+
+    for (const value of values.reverse()) {
+        if (value.startsWith(short)) {
+            return value as Language;
+        }
+    }
+
+    return Language.ENGLISH;
+}
+
+/**
+ * Keeps track of the last open channels, tabs, etc.
+ * Handles providing good UX experience on navigating
+ * back and forth between different parts of the app.
+ */
+export default class LocaleOptions implements Persistent<Data> {
+    private lang: Language;
+
+    /**
+     * Construct new LocaleOptions store.
+     */
+    constructor() {
+        this.lang = findLanguage();
+        makeAutoObservable(this);
+    }
+
+    // eslint-disable-next-line require-jsdoc
+    toJSON() {
+        return {
+            lang: this.lang,
+        };
+    }
+
+    // eslint-disable-next-line require-jsdoc
+    @action hydrate(data: Data) {
+        this.lang = data.lang;
+    }
+
+    /**
+     * Get current language.
+     */
+    @computed getLang() {
+        return this.lang;
+    }
+}
