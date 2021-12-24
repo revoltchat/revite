e9b646f039ce80aa957a1e7750a37cbf3b88c10a
Paul <paulmakles@gmail.com>
2021-12-24 14:49:21 +0000

    fix: hide new messages bar on click

--- src/components/common/messaging/bars/NewMessages.tsx
+++ src/components/common/messaging/bars/NewMessages.tsx
@@ -4,6 +4,8 @@ import { useHistory } from "react-router-dom";
 import { Channel } from "revolt.js/dist/maps/Channels";
 import { decodeTime } from "ulid";
 
+import { useEffect, useState } from "preact/hooks";
+
 import { getRenderer } from "../../../../lib/renderer/Singleton";
 
 import { dayjs } from "../../../../context/Locale";
@@ -12,36 +14,37 @@ import { Bar } from "./JumpToBottom";
 
 export default observer(
     ({ channel, last_id }: { channel: Channel; last_id?: string }) => {
+        const [hidden, setHidden] = useState(false);
+        useEffect(() => setHidden(false), [last_id]);
+
         const renderer = getRenderer(channel);
         const history = useHistory();
         if (renderer.state !== "RENDER") return null;
         if (!last_id) return null;
+        if (hidden) return null;
 
         return (
-            <>
-                <Bar position="top" accent>
-                    <div
-                        onClick={() => {
-                            if (channel.channel_type === "TextChannel") {
-                                history.push(
-                                    `/server/${channel.server_id}/channel/${channel._id}/${last_id}`,
-                                );
-                            } else {
-                                history.push(
-                                    `/channel/${channel._id}/${last_id}`,
-                                );
-                            }
-                        }}>
-                        <div>
-                            New messages since{" "}
-                            {dayjs(decodeTime(last_id)).fromNow()}
-                        </div>
-                        <div>
-                            Click to jump to start. <UpArrowAlt size={20} />
-                        </div>
+            <Bar position="top" accent>
+                <div
+                    onClick={() => {
+                        setHidden(true);
+                        if (channel.channel_type === "TextChannel") {
+                            history.push(
+                                `/server/${channel.server_id}/channel/${channel._id}/${last_id}`,
+                            );
+                        } else {
+                            history.push(`/channel/${channel._id}/${last_id}`);
+                        }
+                    }}>
+                    <div>
+                        New messages since{" "}
+                        {dayjs(decodeTime(last_id)).fromNow()}
                     </div>
-                </Bar>
-            </>
+                    <div>
+                        Click to jump to start. <UpArrowAlt size={20} />
+                    </div>
+                </div>
+            </Bar>
         );
     },
 );
