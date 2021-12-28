d364987d2321d5339391f6832bd671cf0c02a691
trashtemp <96388163+trashtemp@users.noreply.github.com>
2021-12-28 20:43:42 +0100

    chore(header): deprecated old header code

--- src/components/common/ServerHeader.tsx
+++ src/components/common/ServerHeader.tsx
@@ -85,59 +85,53 @@ export default observer(({ server }: Props) => {
                 background: bannerURL ? `url('${bannerURL}')` : undefined,
             }}>
             <div className="container">
-                <div>
-                    {server.flags && server.flags & 1 ? (
-                        <Tooltip
-                            content={
-                                <Text id="app.special.server-badges.official" />
-                            }
-                            placement={"bottom-start"}>
-                            <svg width="20" height="20">
-                                <image
-                                    xlinkHref="/assets/badges/verified.svg"
-                                    height="20"
-                                    width="20"
+                {server.flags && server.flags & 1 ? (
+                    <Tooltip
+                        content={
+                            <Text id="app.special.server-badges.official" />
+                        }
+                        placement={"bottom-start"}>
+                        <svg width="20" height="20">
+                            <image
+                                xlinkHref="/assets/badges/verified.svg"
+                                height="20"
+                                width="20"
+                            />
+                            <image
+                                xlinkHref="/assets/badges/revolt_r.svg"
+                                height="15"
+                                width="15"
+                                x="2"
+                                y="3"
+                                style={
+                                    "justify-content: center; align-items: center; filter: brightness(0);"
+                                }
+                            />
+                        </svg>
+                    </Tooltip>
+                ) : undefined}
+                {server.flags && server.flags & 2 ? (
+                    <Tooltip
+                        content={
+                            <Text id="app.special.server-badges.verified" />
+                        }
+                        placement={"bottom-start"}>
+                        <svg width="20" height="20">
+                            <image
+                                xlinkHref="/assets/badges/verified.svg"
+                                height="20"
+                                width="20"
+                            />
+                            <foreignObject x="2" y="2" width="15" height="15">
+                                <Check
+                                    size={15}
+                                    color="black"
+                                    strokeWidth={8}
                                 />
-                                <image
-                                    xlinkHref="/assets/badges/revolt_r.svg"
-                                    height="15"
-                                    width="15"
-                                    x="2"
-                                    y="3"
-                                    style={
-                                        "justify-content: center; align-items: center; filter: brightness(0);"
-                                    }
-                                />
-                            </svg>
-                        </Tooltip>
-                    ) : undefined}
-                    {server.flags && server.flags & 2 ? (
-                        <Tooltip
-                            content={
-                                <Text id="app.special.server-badges.verified" />
-                            }
-                            placement={"bottom-start"}>
-                            <svg width="20" height="20">
-                                <image
-                                    xlinkHref="/assets/badges/verified.svg"
-                                    height="20"
-                                    width="20"
-                                />
-                                <foreignObject
-                                    x="2"
-                                    y="2"
-                                    width="15"
-                                    height="15">
-                                    <Check
-                                        size={15}
-                                        color="black"
-                                        strokeWidth={8}
-                                    />
-                                </foreignObject>
-                            </svg>
-                        </Tooltip>
-                    ) : undefined}
-                </div>
+                            </foreignObject>
+                        </svg>
+                    </Tooltip>
+                ) : undefined}
                 <div className="title">{server.name}</div>
                 {(server.permission & ServerPermission.ManageServer) > 0 && (
                     <div className="actions">
@@ -150,63 +144,5 @@ export default observer(({ server }: Props) => {
                 )}
             </div>
         </ServerBanner>
-        /*<Header
-            borders
-            placement="secondary"
-            background={typeof bannerURL !== "undefined"}
-            style={{
-                background: bannerURL ? `url('${bannerURL}')` : undefined,
-            }}>
-            {server.flags && server.flags & 1 ? (
-                <Tooltip
-                    content={<Text id="app.special.server-badges.official" />}
-                    placement={"bottom-start"}>
-                    <svg width="20" height="20">
-                        <image
-                            xlinkHref="/assets/badges/verified.svg"
-                            height="20"
-                            width="20"
-                        />
-                        <image
-                            xlinkHref="/assets/badges/revolt_r.svg"
-                            height="15"
-                            width="15"
-                            x="2"
-                            y="3"
-                            style={
-                                "justify-content: center; align-items: center; filter: brightness(0);"
-                            }
-                        />
-                    </svg>
-                </Tooltip>
-            ) : undefined}
-            {server.flags && server.flags & 2 ? (
-                <Tooltip
-                    content={<Text id="app.special.server-badges.verified" />}
-                    placement={"bottom-start"}>
-                    <svg width="20" height="20">
-                        <image
-                            xlinkHref="/assets/badges/verified.svg"
-                            height="20"
-                            width="20"
-                        />
-                        <foreignObject x="2" y="2" width="15" height="15">
-                            <Check size={15} color="black" strokeWidth={8} />
-                        </foreignObject>
-                    </svg>
-                </Tooltip>
-            ) : undefined}
-
-            <ServerName>{server.name}</ServerName>
-            {(server.permission & ServerPermission.ManageServer) > 0 && (
-                <div className="actions">
-                    <Link to={`/server/${server._id}/settings`}>
-                        <IconButton>
-                            <Cog size={24} />
-                        </IconButton>
-                    </Link>
-                </div>
-            )}
-            </Header>*/
     );
 });
