086ab7b3bc9e21e9cfca7e8faf25a3f2183c172d
trashtemp <96388163+trashtemp@users.noreply.github.com>
2021-12-29 16:39:35 +0100

    fix(channel): last update before master merge

--- src/components/common/ServerHeader.tsx
+++ src/components/common/ServerHeader.tsx
@@ -26,9 +26,6 @@ const ServerBanner = styled.div<Props>`
     display: flex;
     flex-direction: column;
     justify-content: end;
-    /*background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;*/
 
     background-size: cover !important;
     background-position: center center !important;
--- src/pages/RevoltApp.tsx
+++ src/pages/RevoltApp.tsx
@@ -24,12 +24,38 @@ import ChannelSettings from "./settings/ChannelSettings";
 import ServerSettings from "./settings/ServerSettings";
 import Settings from "./settings/Settings";
 
+const AppContainer = styled.div`
+    background-size: cover !important;
+    background-position: center center !important;
+`;
+
+const StatusBar = styled.div`
+    height: 40px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 13px;
+    gap: 14px;
+
+    .button {
+        border: 1px solid white;
+        padding: 5px;
+        border-radius: var(--border-radius);
+    }
+`;
+
 const Routes = styled.div`
     min-width: 0;
     display: flex;
     flex-direction: column;
     position: relative;
     background: var(--primary-background);
+    /*background-color: rgba(
+        var(--primary-background-rgb),
+        max(var(--min-opacity), 0.75)
+    );*/
+    //backdrop-filter: blur(10px);
 `;
 
 export default function App() {
@@ -44,91 +70,103 @@ export default function App() {
 
     return (
         <>
-            {window.isNative && !window.native.getConfig().frame && (
-                <Titlebar />
-            )}
-            <OverlappingPanels
-                width="100vw"
-                height={
-                    window.isNative && !window.native.getConfig().frame
-                        ? "calc(var(--app-height) - var(--titlebar-height))"
-                        : "var(--app-height)"
-                }
-                leftPanel={
-                    inSpecial
-                        ? undefined
-                        : { width: 288, component: <LeftSidebar /> }
-                }
-                rightPanel={
-                    !inSpecial && inChannel
-                        ? { width: 236, component: <RightSidebar /> }
-                        : undefined
-                }
-                bottomNav={{
-                    component: <BottomNavigation />,
-                    showIf: fixedBottomNav ? ShowIf.Always : ShowIf.Left,
-                    height: 50,
-                }}
-                docked={isTouchscreenDevice ? Docked.None : Docked.Left}>
-                <Routes>
-                    <Switch>
-                        <Route
-                            path="/server/:server/channel/:channel/settings/:page"
-                            component={ChannelSettings}
-                        />
-                        <Route
-                            path="/server/:server/channel/:channel/settings"
-                            component={ChannelSettings}
-                        />
-                        <Route
-                            path="/server/:server/settings/:page"
-                            component={ServerSettings}
-                        />
-                        <Route
-                            path="/server/:server/settings"
-                            component={ServerSettings}
-                        />
-                        <Route
-                            path="/channel/:channel/settings/:page"
-                            component={ChannelSettings}
-                        />
-                        <Route
-                            path="/channel/:channel/settings"
-                            component={ChannelSettings}
-                        />
+            {/*<StatusBar>
+                <div className="title">Planned outage: CDN (~2 hours)</div>
+                <div className="button">View status</div>
+            </StatusBar>*/}
+            <AppContainer>
+                {window.isNative && !window.native.getConfig().frame && (
+                    <Titlebar />
+                )}
+                <OverlappingPanels
+                    width="100vw"
+                    height={
+                        window.isNative && !window.native.getConfig().frame
+                            ? "calc(var(--app-height) - var(--titlebar-height))"
+                            : "var(--app-height)"
+                    }
+                    leftPanel={
+                        inSpecial
+                            ? undefined
+                            : { width: 288, component: <LeftSidebar /> }
+                    }
+                    rightPanel={
+                        !inSpecial && inChannel
+                            ? { width: 236, component: <RightSidebar /> }
+                            : undefined
+                    }
+                    bottomNav={{
+                        component: <BottomNavigation />,
+                        showIf: fixedBottomNav ? ShowIf.Always : ShowIf.Left,
+                        height: 50,
+                    }}
+                    docked={isTouchscreenDevice ? Docked.None : Docked.Left}>
+                    <Routes>
+                        <Switch>
+                            <Route
+                                path="/server/:server/channel/:channel/settings/:page"
+                                component={ChannelSettings}
+                            />
+                            <Route
+                                path="/server/:server/channel/:channel/settings"
+                                component={ChannelSettings}
+                            />
+                            <Route
+                                path="/server/:server/settings/:page"
+                                component={ServerSettings}
+                            />
+                            <Route
+                                path="/server/:server/settings"
+                                component={ServerSettings}
+                            />
+                            <Route
+                                path="/channel/:channel/settings/:page"
+                                component={ChannelSettings}
+                            />
+                            <Route
+                                path="/channel/:channel/settings"
+                                component={ChannelSettings}
+                            />
 
-                        <Route
-                            path="/channel/:channel/:message"
-                            component={Channel}
-                        />
-                        <Route
-                            path="/server/:server/channel/:channel/:message"
-                            component={Channel}
-                        />
+                            <Route
+                                path="/channel/:channel/:message"
+                                component={Channel}
+                            />
+                            <Route
+                                path="/server/:server/channel/:channel/:message"
+                                component={Channel}
+                            />
 
-                        <Route
-                            path="/server/:server/channel/:channel"
-                            component={Channel}
-                        />
-                        <Route path="/server/:server" component={Channel} />
-                        <Route path="/channel/:channel" component={Channel} />
+                            <Route
+                                path="/server/:server/channel/:channel"
+                                component={Channel}
+                            />
+                            <Route path="/server/:server" component={Channel} />
+                            <Route
+                                path="/channel/:channel"
+                                component={Channel}
+                            />
 
-                        <Route path="/settings/:page" component={Settings} />
-                        <Route path="/settings" component={Settings} />
+                            <Route
+                                path="/settings/:page"
+                                component={Settings}
+                            />
+                            <Route path="/settings" component={Settings} />
 
-                        <Route path="/dev" component={Developer} />
-                        <Route path="/friends" component={Friends} />
-                        <Route path="/open/:id" component={Open} />
-                        <Route path="/bot/:id" component={InviteBot} />
-                        <Route path="/invite/:code?" component={Invite} />
-                        <Route path="/" component={Home} />
-                    </Switch>
-                </Routes>
-                <ContextMenus />
-                <Popovers />
-                <Notifications />
-                <StateMonitor />
-            </OverlappingPanels>
+                            <Route path="/dev" component={Developer} />
+                            <Route path="/friends" component={Friends} />
+                            <Route path="/open/:id" component={Open} />
+                            <Route path="/bot/:id" component={InviteBot} />
+                            <Route path="/invite/:code?" component={Invite} />
+                            <Route path="/" component={Home} />
+                        </Switch>
+                    </Routes>
+                    <ContextMenus />
+                    <Popovers />
+                    <Notifications />
+                    <StateMonitor />
+                </OverlappingPanels>
+            </AppContainer>
         </>
     );
 }
