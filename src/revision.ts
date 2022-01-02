a688063d96a1db455621d87bee4333cba5a79898
Paul Makles <paulmakles@gmail.com>
2022-01-02 15:33:58 +0000

    feat: add better debugging flags

--- src/revision.ts
+++ src/revision.ts
@@ -4,3 +4,7 @@
 export const REPO_URL: string = "https://github.com/revoltchat/revite/commit";
 export const GIT_REVISION: string = "__GIT_REVISION__";
 export const GIT_BRANCH: string = "__GIT_BRANCH__";
+
+export function isDebug() {
+    return import.meta.env.FORCE_DEBUG === "1" || import.meta.env.DEV;
+}
