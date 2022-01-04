dfd9043ca029721725b33189228679f396ee7aee
trashtemp <96388163+trashtemp@users.noreply.github.com>
2022-01-04 20:00:06 +0100

    fix(app): tweaked glass value for few components

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -54,7 +54,7 @@ const Header = styled.div<Props>`
                       var(--primary-header-rgb),
                       max(var(--min-opacity), 0.75)
                   );
-                  backdrop-filter: blur(10px);
+                  backdrop-filter: blur(20px);
                   z-index: 20;
                   position: absolute;
                   width: 100%;
