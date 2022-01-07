5b10619c15007dd718934da51f26eca9e8199ec5
Kuhn Chris <kuhnchris+github@kuhnchris.eu>
2022-01-07 23:39:50 +0100

    Style; permission based btns for edit and delete

--- src/components/common/messaging/bars/MessageOverlayBar.tsx
+++ src/components/common/messaging/bars/MessageOverlayBar.tsx
@@ -1,24 +1,39 @@
-import { DotsHorizontalRounded, Reply } from "@styled-icons/boxicons-regular";
+import {
+    DotsHorizontalRounded,
+    Edit,
+    Reply,
+    Trash,
+} from "@styled-icons/boxicons-regular";
 import { observer } from "mobx-react-lite";
 import { Message as MessageObject } from "revolt.js/dist/maps/Messages";
 import styled, { css } from "styled-components";
 
 import { attachContextMenu } from "preact-context-menu";
+import { useContext } from "preact/hooks";
 
 import { internalEmit } from "../../../../lib/eventEmitter";
 
 import { QueuedMessage } from "../../../../mobx/stores/MessageQueue";
 
+import {
+    Screen,
+    useIntermediate,
+} from "../../../../context/intermediate/Intermediate";
+import { AppContext } from "../../../../context/revoltjs/RevoltClient";
+
 import IconButton from "../../../ui/IconButton";
+import LineDivider from "../../../ui/LineDivider";
 
 import { Children } from "../../../../types/Preact";
+
+const OverlayButtonWidth = 25;
 
 export const OverlayBase = styled.div`
     position: relative;
 `;
 
 export const OverlayBarFlexItemStyled = styled.div`
-    width: 25px;
+    width: ${OverlayButtonWidth}px;
     height: 25px;
     opacity: 75%;
 `;
@@ -47,35 +62,62 @@ export const OverlayBarFlexItem = observer(
 
 export interface OverlayBarProps {
     visible?: boolean;
+    canEdit?: boolean;
+    canDelete?: boolean;
 }
 export const OverlayBar = styled.div<OverlayBarProps>`
     ${(props) =>
         props.visible === true &&
         css`
-            display: inline-flex;
+            opacity: 1;
         `};
     ${(props) =>
         props.visible !== true &&
         css`
-            display: none;
+            opacity: 0;
         `};
-
+    display: inline-flex;
     position: absolute;
-    justify-content: end;
     justify-self: end;
     align-self: end;
-    align-items: end;
+    align-content: center;
+    background: var(--primary-header);
     top: 0;
     right: 0;
     z-index: 0;
-    width: 50px;
+    width: ${(props) =>
+        OverlayButtonWidth * 2 +
+        (props.canEdit === true ? OverlayButtonWidth : 0) +
+        (props.canDelete === true ? OverlayButtonWidth : 0)}px;
+    border: 2px;
+    border-style: solid;
+    border-color: rgba(255, 255, 255, 0.25);
+    border-radius: 5px;
 `;
 
 export const MessageOverlayBar = observer(
     ({ message, queued, mouseOver }: MessageOverlayBarProps) => {
+        const { openScreen, writeClipboard } = useIntermediate();
+
+        const client = useContext(AppContext);
+        const userId = client.user!._id;
+        const userIsAuthor = message.author_id === userId;
+
         const handleReplyQuickAction = () => {
             internalEmit("ReplyBar", "add", message);
         };
+        const handleEditQuickAction = () => {
+            internalEmit("MessageRenderer", "edit_message", message._id);
+        };
+        const handleDeleteQuickAction = () => {
+            // Typescript flattens the case types into a single type and type structure and specifity is lost
+            openScreen({
+                id: "special_prompt",
+                type: "delete_message",
+                target: message,
+            } as unknown as Screen);
+        };
+
         const handleMoreQuickAction = (e: any) => {
             attachContextMenu("Menu", {
                 message,
@@ -83,12 +125,29 @@ export const MessageOverlayBar = observer(
                 queued,
             })(e);
         };
+
         return (
-            <OverlayBar visible={mouseOver}>
+            <OverlayBar
+                visible={mouseOver}
+                canDelete={userIsAuthor}
+                canEdit={userIsAuthor}>
                 <OverlayBarFlexItem
                     onClick={handleReplyQuickAction}
                     iconContent={<Reply />}
                 />
+                {userIsAuthor && (
+                    <OverlayBarFlexItem
+                        onClick={handleEditQuickAction}
+                        iconContent={<Edit />}
+                    />
+                )}
+                {userIsAuthor && (
+                    <OverlayBarFlexItem
+                        onClick={handleDeleteQuickAction}
+                        iconContent={<Trash />}
+                    />
+                )}
+                |
                 <OverlayBarFlexItem
                     onClick={handleMoreQuickAction}
                     iconContent={<DotsHorizontalRounded />}
