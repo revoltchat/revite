e959f29eae0beb5345b07950b606c7fb737b6ccd
trashtemp <96388163+trashtemp@users.noreply.github.com>
2022-01-21 00:26:06 +0100

    fix(reply): used var instead of fixed hex

--- src/components/common/messaging/attachments/MessageReply.tsx
+++ src/components/common/messaging/attachments/MessageReply.tsx
@@ -42,8 +42,8 @@ export const ReplyBase = styled.div<{
         content: "";
         width: 22px;
         height: 10px;
-        border-inline-start: 3px solid #4f515d;
-        border-top: 3px solid #4f515d;
+        border-inline-start: 3px solid var(--message-box);
+        border-top: 3px solid var(--message-box);
         align-self: flex-end;
     }
 
