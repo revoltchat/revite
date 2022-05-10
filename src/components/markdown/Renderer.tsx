8f0c69f5282fcdbdb0ea99864152de8e4138bb10
Paul Makles <paulmakles@gmail.com>
2022-05-10 12:27:44 +0100

    chore: remove super and subtext (closes #438)

--- src/components/markdown/Renderer.tsx
+++ src/components/markdown/Renderer.tsx
@@ -5,10 +5,6 @@ import "katex/dist/katex.min.css";
 import MarkdownIt from "markdown-it";
 // @ts-expect-error No typings.
 import MarkdownEmoji from "markdown-it-emoji/dist/markdown-it-emoji-bare";
-// @ts-expect-error No typings.
-import MarkdownSub from "markdown-it-sub";
-// @ts-expect-error No typings.
-import MarkdownSup from "markdown-it-sup";
 import { RE_MENTIONS } from "revolt.js";
 
 import styles from "./Markdown.module.scss";
@@ -64,8 +60,6 @@ export const md: MarkdownIt = MarkdownIt({
     .disable("image")
     .use(MarkdownEmoji, { defs: emojiDictionary })
     .use(MarkdownSpoilers)
-    .use(MarkdownSup)
-    .use(MarkdownSub)
     .use(MarkdownKatex, {
         throwOnError: false,
         maxExpand: 0,
