81bf3259900b969c754a1c3a08571d6abfec08b9
Paul Makles <paulmakles@gmail.com>
2022-05-30 16:15:52 +0100

    feat: add column element

--- src/pages/settings/panes/Notifications.tsx
+++ src/pages/settings/panes/Notifications.tsx
@@ -4,7 +4,7 @@ import styles from "./Panes.module.scss";
 import { Text } from "preact-i18n";
 import { useContext, useEffect, useState } from "preact/hooks";
 
-import { Checkbox } from "@revoltchat/ui";
+import { Checkbox, Column } from "@revoltchat/ui";
 
 import { urlBase64ToUint8Array } from "../../../lib/conversion";
 
@@ -36,96 +36,103 @@ export const Notifications = observer(() => {
             <h3>
                 <Text id="app.settings.pages.notifications.push_notifications" />
             </h3>
-            <Checkbox
-                disabled={!("Notification" in window)}
-                value={settings.get("notifications:desktop", false)!}
-                title={
-                    <Text id="app.settings.pages.notifications.enable_desktop" />
-                }
-                description={
-                    <Text id="app.settings.pages.notifications.descriptions.enable_desktop" />
-                }
-                onChange={async (desktopEnabled) => {
-                    if (desktopEnabled) {
-                        const permission =
-                            await Notification.requestPermission();
-
-                        if (permission !== "granted") {
-                            return openScreen({
-                                id: "error",
-                                error: "DeniedNotification",
-                            });
-                        }
+            <Column>
+                <Checkbox
+                    disabled={!("Notification" in window)}
+                    value={settings.get("notifications:desktop", false)!}
+                    title={
+                        <Text id="app.settings.pages.notifications.enable_desktop" />
                     }
+                    description={
+                        <Text id="app.settings.pages.notifications.descriptions.enable_desktop" />
+                    }
+                    onChange={async (desktopEnabled) => {
+                        if (desktopEnabled) {
+                            const permission =
+                                await Notification.requestPermission();
 
-                    settings.set("notifications:desktop", desktopEnabled);
-                }}
-            />
-            <Checkbox
-                disabled={typeof pushEnabled === "undefined"}
-                value={pushEnabled ?? false}
-                title={
-                    <Text id="app.settings.pages.notifications.enable_push" />
-                }
-                description={
-                    <Text id="app.settings.pages.notifications.descriptions.enable_push" />
-                }
-                onChange={async (pushEnabled) => {
-                    try {
-                        const reg =
-                            await navigator.serviceWorker?.getRegistration();
-                        if (reg) {
-                            if (pushEnabled) {
-                                const sub = await reg.pushManager.subscribe({
-                                    userVisibleOnly: true,
-                                    applicationServerKey: urlBase64ToUint8Array(
-                                        client.configuration!.vapid,
-                                    ),
+                            if (permission !== "granted") {
+                                return openScreen({
+                                    id: "error",
+                                    error: "DeniedNotification",
                                 });
-
-                                // tell the server we just subscribed
-                                const json = sub.toJSON();
-                                if (json.keys) {
-                                    client.api.post("/push/subscribe", {
-                                        endpoint: sub.endpoint,
-                                        ...(json.keys as {
-                                            p256dh: string;
-                                            auth: string;
-                                        }),
-                                    });
-                                    setPushEnabled(true);
-                                }
-                            } else {
-                                const sub =
-                                    await reg.pushManager.getSubscription();
-                                sub?.unsubscribe();
-                                setPushEnabled(false);
-
-                                client.api.post("/push/unsubscribe");
                             }
                         }
-                    } catch (err) {
-                        console.error("Failed to enable push!", err);
+
+                        settings.set("notifications:desktop", desktopEnabled);
+                    }}
+                />
+                <Checkbox
+                    disabled={typeof pushEnabled === "undefined"}
+                    value={pushEnabled ?? false}
+                    title={
+                        <Text id="app.settings.pages.notifications.enable_push" />
                     }
-                }}
-            />
+                    description={
+                        <Text id="app.settings.pages.notifications.descriptions.enable_push" />
+                    }
+                    onChange={async (pushEnabled) => {
+                        try {
+                            const reg =
+                                await navigator.serviceWorker?.getRegistration();
+                            if (reg) {
+                                if (pushEnabled) {
+                                    const sub = await reg.pushManager.subscribe(
+                                        {
+                                            userVisibleOnly: true,
+                                            applicationServerKey:
+                                                urlBase64ToUint8Array(
+                                                    client.configuration!.vapid,
+                                                ),
+                                        },
+                                    );
+
+                                    // tell the server we just subscribed
+                                    const json = sub.toJSON();
+                                    if (json.keys) {
+                                        client.api.post("/push/subscribe", {
+                                            endpoint: sub.endpoint,
+                                            ...(json.keys as {
+                                                p256dh: string;
+                                                auth: string;
+                                            }),
+                                        });
+                                        setPushEnabled(true);
+                                    }
+                                } else {
+                                    const sub =
+                                        await reg.pushManager.getSubscription();
+                                    sub?.unsubscribe();
+                                    setPushEnabled(false);
+
+                                    client.api.post("/push/unsubscribe");
+                                }
+                            }
+                        } catch (err) {
+                            console.error("Failed to enable push!", err);
+                        }
+                    }}
+                />
+            </Column>
             <h3>
                 <Text id="app.settings.pages.notifications.sounds" />
             </h3>
-            {settings.sounds.getState().map(({ id, enabled }) => (
-                <Checkbox
-                    key={id}
-                    value={enabled}
-                    title={
-                        <Text
-                            id={`app.settings.pages.notifications.sound.${id}`}
-                        />
-                    }
-                    onChange={(enabled) =>
-                        settings.sounds.setEnabled(id, enabled)
-                    }
-                />
-            ))}
+            <Column>
+                {settings.sounds.getState().map(({ id, enabled }) => (
+                    <Checkbox
+                        key={id}
+                        value={enabled}
+                        title={
+                            <Text
+                                id={`app.settings.pages.notifications.sound.${id}`}
+                            />
+                        }
+                        onChange={(enabled) =>
+                            settings.sounds.setEnabled(id, enabled)
+                        }
+                    />
+                ))}
+            </Column>
         </div>
     );
 });
--- src/pages/settings/panes/Sync.tsx
+++ src/pages/settings/panes/Sync.tsx
@@ -3,7 +3,7 @@ import { observer } from "mobx-react-lite";
 import styles from "./Panes.module.scss";
 import { Text } from "preact-i18n";
 
-import { Checkbox } from "@revoltchat/ui";
+import { Checkbox, Column } from "@revoltchat/ui";
 
 import { useApplicationState } from "../../../mobx/State";
 import { SyncKeys } from "../../../mobx/stores/Sync";
@@ -20,26 +20,28 @@ export const Sync = observer(() => {
             <h3>
                 <Text id="app.settings.pages.sync.categories" />
             </h3>
-            {(
-                [
-                    ["appearance", "appearance.title"],
-                    ["theme", "appearance.theme"],
-                    ["locale", "language.title"],
-                    // notifications sync is always-on
-                ] as [SyncKeys, string][]
-            ).map(([key, title]) => (
-                <Checkbox
-                    key={key}
-                    value={sync.isEnabled(key)}
-                    title={<Text id={`app.settings.pages.${title}`} />}
-                    description={
-                        <Text
-                            id={`app.settings.pages.sync.descriptions.${key}`}
-                        />
-                    }
-                    onChange={() => sync.toggle(key)}
-                />
-            ))}
+            <Column>
+                {(
+                    [
+                        ["appearance", "appearance.title"],
+                        ["theme", "appearance.theme"],
+                        ["locale", "language.title"],
+                        // notifications sync is always-on
+                    ] as [SyncKeys, string][]
+                ).map(([key, title]) => (
+                    <Checkbox
+                        key={key}
+                        value={sync.isEnabled(key)}
+                        title={<Text id={`app.settings.pages.${title}`} />}
+                        description={
+                            <Text
+                                id={`app.settings.pages.sync.descriptions.${key}`}
+                            />
+                        }
+                        onChange={() => sync.toggle(key)}
+                    />
+                ))}
+            </Column>
             {/*<h5 style={{ marginTop: "20px", color: "grey" }}>
                 Last sync at 12:00
                 </h5>*/}
