834f59a97a7d631671b226d475626fd0ab4e409d
Leda <ledathemis@protonmail.com>
2022-06-04 23:01:39 +0000

    chore: format

--- src/components/navigation/items/ButtonItem.tsx
+++ src/components/navigation/items/ButtonItem.tsx
@@ -14,14 +14,13 @@ import { stopPropagation } from "../../../lib/stopPropagation";
 
 import { useIntermediate } from "../../../context/intermediate/Intermediate";
 
+import { Children } from "../../../types/Preact";
 import ChannelIcon from "../../common/ChannelIcon";
 import Tooltip from "../../common/Tooltip";
 import UserIcon from "../../common/user/UserIcon";
 import { Username } from "../../common/user/UserShort";
 import UserStatus from "../../common/user/UserStatus";
 import IconButton from "../../ui/IconButton";
-
-import { Children } from "../../../types/Preact";
 
 type CommonProps = Omit<
     JSX.HTMLAttributes<HTMLDivElement>,
