fc0c7611d433354a5aae82ceb72bb1ca7fe4a4f1
Leda <38331868+LedaThemis@users.noreply.github.com>
2022-06-14 14:23:58 +0000

    fix: bug where channel icon scales with channel name (#661)
    Co-authored-by: Paul Makles <paulmakles@gmail.com>

--- src/components/navigation/items/ButtonItem.tsx
+++ src/components/navigation/items/ButtonItem.tsx
@@ -15,11 +15,13 @@ import { stopPropagation } from "../../../lib/stopPropagation";
 
 import { useIntermediate } from "../../../context/intermediate/Intermediate";
 
+import { Children } from "../../../types/Preact";
 import ChannelIcon from "../../common/ChannelIcon";
 import Tooltip from "../../common/Tooltip";
 import UserIcon from "../../common/user/UserIcon";
 import { Username } from "../../common/user/UserShort";
 import UserStatus from "../../common/user/UserStatus";
+import IconButton from "../../ui/IconButton";
 
 type CommonProps = Omit<
     JSX.HTMLAttributes<HTMLDivElement>,
@@ -164,11 +166,9 @@ export const ChannelButton = observer((props: ChannelProps) => {
                 channel: channel._id,
                 unread: !!alert,
             })}>
-            <ChannelIcon
-                className={styles.avatar}
-                target={channel}
-                size={compact ? 24 : 32}
-            />
+            <div className={styles.avatar}>
+                <ChannelIcon target={channel} size={compact ? 24 : 32} />
+            </div>
             <div className={styles.name}>
                 <div>{channel.name}</div>
                 {channel.channel_type === "Group" && (
