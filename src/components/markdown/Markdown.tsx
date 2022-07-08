4a85dd69cf66dcbf654cd67ba11db80c033ea90e
Paul Makles <paulmakles@gmail.com>
2022-07-08 15:19:16 +0100

    fix: limit maximum quote depth to 3

--- src/components/markdown/Markdown.tsx
+++ src/components/markdown/Markdown.tsx
@@ -3,11 +3,13 @@ import { Suspense, lazy } from "preact/compat";
 const Renderer = lazy(() => import("./RemarkRenderer"));
 
 export interface MarkdownProps {
-    content?: string | null;
+    content: string;
     disallowBigEmoji?: boolean;
 }
 
 export default function Markdown(props: MarkdownProps) {
+    if (!props.content) return null;
+
     return (
         // @ts-expect-error Typings mis-match.
         <Suspense fallback={props.content}>
