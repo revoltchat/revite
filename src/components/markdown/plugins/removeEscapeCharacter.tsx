4481c8686643d2f2b6c90b3fefeec8bb05f4a77b
Leda <ledathemis@protonmail.com>
2022-07-14 21:12:04 +0000

    fix: handle code blocks with html inside correctly
    typescript doesn't like the ["text", "code"] syntax, check https://github.com/syntax-tree/unist-util-visit/issues/32

--- src/components/markdown/plugins/removeEscapeCharacter.tsx
+++ src/components/markdown/plugins/removeEscapeCharacter.tsx
@@ -5,10 +5,12 @@ export const ESCAPE_CHARACTER = `${Math.random()}`;
 
 export const remarkRemoveEscapeCharacter: Plugin = () => {
     return (tree) => {
-        visit(tree, "", (node: { value: string }) => {
-            if (node.value === ESCAPE_CHARACTER) {
+        visit(tree, "", (node: { value: string; type: string }) => {
+            if (node.value === ESCAPE_CHARACTER && node.type === 'text') {
                 node.value = '';
-            }
+              } else if (node.value.includes(ESCAPE_CHARACTER) && node.type === 'code') {
+                node.value = node.value.replaceAll(ESCAPE_CHARACTER, '');
+              }
         });
     };
 };
