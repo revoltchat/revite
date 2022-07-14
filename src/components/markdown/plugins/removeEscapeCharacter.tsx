7d3f111ab71a478a0e155541d4210412d357cccd
Leda <ledathemis@protonmail.com>
2022-07-14 21:49:43 +0000

    chore: format, add docstring

--- src/components/markdown/plugins/removeEscapeCharacter.tsx
+++ src/components/markdown/plugins/removeEscapeCharacter.tsx
@@ -1,16 +1,22 @@
 import { Plugin } from "unified";
 import { visit } from "unist-util-visit";
 
+/**
+ * Randomly generated string to escape HTML tags
+ */
 export const ESCAPE_CHARACTER = `${Math.random()}`;
 
 export const remarkRemoveEscapeCharacter: Plugin = () => {
     return (tree) => {
         visit(tree, "", (node: { value: string; type: string }) => {
-            if (node.value === ESCAPE_CHARACTER && node.type === 'text') {
-                node.value = '';
-              } else if (node.value.includes(ESCAPE_CHARACTER) && node.type === 'code') {
-                node.value = node.value.replaceAll(ESCAPE_CHARACTER, '');
-              }
+            if (node.value === ESCAPE_CHARACTER && node.type === "text") {
+                node.value = "";
+            } else if (
+                node.value.includes(ESCAPE_CHARACTER) &&
+                node.type === "code"
+            ) {
+                node.value = node.value.replaceAll(ESCAPE_CHARACTER, "");
+            }
         });
     };
 };
