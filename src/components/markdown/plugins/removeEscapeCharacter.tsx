610568dd57c54385b35926032b9adf04e3a01d5f
Leda <ledathemis@protonmail.com>
2022-07-14 22:08:51 +0000

    fix: crash caused by node.value being undefined

--- src/components/markdown/plugins/removeEscapeCharacter.tsx
+++ src/components/markdown/plugins/removeEscapeCharacter.tsx
@@ -9,11 +9,11 @@ export const ESCAPE_CHARACTER = `${Math.random()}`;
 export const remarkRemoveEscapeCharacter: Plugin = () => {
     return (tree) => {
         visit(tree, "", (node: { value: string; type: string }) => {
-            if (node.value === ESCAPE_CHARACTER && node.type === "text") {
+            if (node.type === "text" && node.value === ESCAPE_CHARACTER) {
                 node.value = "";
             } else if (
-                node.value.includes(ESCAPE_CHARACTER) &&
-                node.type === "code"
+                node.type === "code" &&
+                node.value.includes(ESCAPE_CHARACTER)
             ) {
                 node.value = node.value.replaceAll(ESCAPE_CHARACTER, "");
             }
