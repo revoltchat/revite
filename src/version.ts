787d5840d2f59bb6c3a4fcec754d45665f4a03cb
Paul Makles <paulmakles@gmail.com>
2022-08-15 19:32:02 +0200

    chore: hide discovery button if not pointing to Revolt

--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,6 @@
 export const APP_VERSION = "__APP_VERSION__";
+export const IS_REVOLT =
+    import.meta.env.VITE_API_URL === "https://api.revolt.chat" ||
+    // future proofing
+    import.meta.env.VITE_API_URL === "https://app.revolt.chat/api" ||
+    import.meta.env.VITE_API_URL === "https://revolt.chat/api";
