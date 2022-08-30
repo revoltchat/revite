a935a83c9c3d993875635bd0be0938355b62d824
Jakub D <jakub@bluenation.studio>
2022-08-30 16:10:48 +0200

    Add android app

---
+++ android/app/src/main/java/chat/revolt/app/MainActivity.java
@@ -0,0 +1,5 @@
+package chat.revolt.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'chat.revolt.app',
+  appName: 'revolt',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
