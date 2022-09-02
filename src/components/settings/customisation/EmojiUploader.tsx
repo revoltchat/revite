7626a1f4616fb09bc58ac75aa59df622536575a8
Paul Makles <paulmakles@gmail.com>
2022-09-02 14:40:17 +0100

    fix: remove stray text

--- src/components/settings/customisation/EmojiUploader.tsx
+++ src/components/settings/customisation/EmojiUploader.tsx
@@ -19,7 +19,6 @@ export function EmojiUploader({ server }: Props) {
             <h3>
                 <Text id="app.settings.server_pages.emojis.upload" />
             </h3>
-            {fileId}
             <Form
                 schema={{
                     name: "text",
