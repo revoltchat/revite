e48bc58a8d7edff5d5df8b16080f099c2509848e
Skythrew <76571984+Skythrew@users.noreply.github.com>
2022-09-19 18:18:40 +0200

    Update EmbedSpoiler.tsx

--- src/components/common/messaging/embed/EmbedSpoiler.tsx
+++ src/components/common/messaging/embed/EmbedSpoiler.tsx
@@ -1,10 +1,15 @@
 import classNames from "classnames";
+import { useState } from "preact/hooks";
 import { API } from "revolt.js";
 import styles from "./Embed.module.scss";
 
+
 export default function EmbedSpoiler(){
+    
+    const [visible, setVisible] = useState(true);
+
     return (
-    <div className={classNames(styles.spoiler_container)}>
+    <div className={classNames(styles.spoiler_container, visible ? null : styles.hidden)} onClick={() => setVisible(!visible)}>
         <h1 className={classNames(styles.spoiler_title)}>SPOILER</h1>
     </div>
     );
