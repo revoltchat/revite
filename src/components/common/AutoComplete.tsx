6062a361f1d78031b07c9921d8de7141ca55b350
kate <kate@dariox.club>
2023-02-05 21:45:32 +0800

    fix: allow dash char in autocorrect content validation (#838)

--- src/components/common/AutoComplete.tsx
+++ src/components/common/AutoComplete.tsx
@@ -64,7 +64,7 @@ export function useAutoComplete(
             const cursor = el.selectionStart;
             const content = el.value.slice(0, cursor);
 
-            const valid = /\w/;
+            const valid = /[\w\-]/;
 
             let j = content.length - 1;
             if (content[j] === "@") {
