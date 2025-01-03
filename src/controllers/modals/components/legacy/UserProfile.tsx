ed6e0f3fbe7b27410ff180032462fde85e2d6660
teamabron <110968521+teamabron@users.noreply.github.com>
2025-01-03 22:16:44 +0330

    Update UserProfile.tsx

--- src/controllers/modals/components/legacy/UserProfile.tsx
+++ src/controllers/modals/components/legacy/UserProfile.tsx
@@ -207,7 +207,7 @@ export const UserProfile = observer(
                                 </Button>
                             </Link>
                         )}
-                        {(user.relationship === "Friend" || user.bot || user.relationship === "None") && (
+						{(user.relationship === "Friend" || user.relationship === "None" || user.relationship === "Outgoing" || user.relationship === "Incoming" || user.bot ) && (
 
                             <Localizer>
                                 <Tooltip
